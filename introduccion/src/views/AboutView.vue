3287dd19076037006b8dc0ddbecdb513e8bbd298
sergio <sergiomijael@gmail.com>
2026-03-24 21:36:37 -0400

    primer commit

---
+++ introduccion/src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is an <span>about</span> page</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.about{
+  color: orangered;
+  span {
+    color:white;
+  }
+}
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
