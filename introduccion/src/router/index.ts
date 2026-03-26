579238053f9644234db6d6f331a2b0b9953dbd1d
sergio <sergiomijael@gmail.com>
2026-03-25 22:16:58 -0400

    25-03-26

--- introduccion/src/router/index.ts
+++ introduccion/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Computed from '../components/Computed.vue'
+import Interpolacion from '@/components/Interpolacion.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/computada',
       name: 'computada',
       component: Computed,
+    },
+    {
+      path: '/interpolacion',
+      name: 'interpolacion',
+      component: Interpolacion,
     },
     {
       path: '/about',
